f64ee1e1ea029e01a8005a62dc856f59c3665d35
Anthony <ab.bulot@gmail.com>
2018-03-06 14:46:04 +0100

    Correction erreur verif form

--- critique/view/formPasswordView.php
+++ critique/view/formPasswordView.php
@@ -10,12 +10,12 @@
 			<label for="passwordNew">Nouveau mot de passe</label>
 			<input type="password" name="passwordNew" class="form-control" id="newPassword" />
 			<p id="aide1"></p>
-			<p id="aide2"></p>
+			<p id="aide2" class="noSubmit"></p>
 		</div>
     	<div class="form-group">
      	    <label for="passwordNew2">Répéter le nouveau mot de passe</label>
         	<input type="password" name="passwordNew2" class="form-control" id="newPassword2" />
-        	<p id="newMdpIdentique"></p>
+        	<p id="newMdpIdentique" class="noSubmit"></p>
     	</div>
     	<input class="btn btn-default" type="submit" name="submit" value="Connexion"/>
 	</form>
