41c3521bd3056047f17352c87868c546ca913309
Anthony <ab.bulot@gmail.com>
2018-03-12 16:05:57 +0100

    essai carouselle bootstraps

--- critique/view/homeView.php
+++ critique/view/homeView.php
@@ -1,21 +1,44 @@
 <?php $template = 'template'; ?>
 <div class="row">
-	<p class="col-lg-offset-2 col-lg-1 col-md-1 col-sm-1" id="left"><<</p>
-	<div class=" col-lg-6 col-md-10 col-sm-10 image">
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  		<ol class="carousel-indicators">
+    		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  		</ol>
+  		<div class="carousel-inner">
 	<?php
 	while($url_image = $url->fetch()){
 		if(!isset($i)){
 			$i=1;
 		}	
 		if ($i == 1) { ?>
-			<p class="center"><a href="/critique/film/<?= $url_image['id'] ?>" class="lienDiapo" id="lienMaPhoto" ><img class="imageHome diapo img-thumbnail" id="maPhoto" src="css/poster/<?= $url_image['url_image'] ?>" alt="Affiche de <?= $url_image['title'] ?>"></a></p>
+			<div class="carousel-item active">
+      			<img class="imageHome center d-block w-100" src="css/poster/<?= $url_image['url_image'] ?>" alt="Affiche de <?= $url_image['title'] ?>">
+      			<a href="/critique/film/<?= $url_image['id'] ?>" class="lienDiapo" id="lienMaPhoto" ></a>
+    		</div>
 		<?php } else { ?>
-			<p class="center"><a href="/critique/film/<?= $url_image['id'] ?>" class="lienDiapo"><img class="imageHome diapo cache img-thumbnail" src="css/poster/<?= $url_image['url_image'] ?>" alt="Affiche de <?= $url_image['title'] ?>"></a></p>
+			<div class="carousel-item">
+      			<img class="imageHome center d-block w-100" src="css/poster/<?= $url_image['url_image'] ?>" alt="Affiche de <?= $url_image['title'] ?>">
+      			<a href="/critique/film/<?= $url_image['id'] ?>" class="lienDiapo"></a>
+    		</div>
 		<?php }
 		$i++;
 	} ?>
+		</div>
+  		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+   	 		<span class="sr-only">Previous</span>
+  		</a>
+  		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    		<span class="sr-only">Next</span>
+  		</a>
 	</div>
-	<p class="col-lg-1 col-md-1 col-sm-1" id="right">>></p>
 </div>
 
-<script src="js/diaporama.js"></script>
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+	$('.carousel').carousel()
+</script>
